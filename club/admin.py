5bca099e24c2b3bafe298b5260dfbe0f20ea7c83
Grimwood73 <jonah.byrne@gmail.com>
2022-04-25 20:03:14 -0700

    Models commit

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Meeting, MeetingMinutes, Resource, Event
+
+admin.site.register(Meeting)
+admin.site.register(MeetingMinutes)
+admin.site.register(Resource)
+admin.site.register(Event)
+
 
 # Register your models here.
--- club/models.py
+++ club/models.py
@@ -1,3 +1,63 @@
+from datetime import date
+#from logging.config import _LoggerConfiguration
+from time import time
 from django.db import models
+from django.contrib.auth.models import User
+
+class Meeting(models.Model):
+    meetingtitle=models.CharField(max_length=255)
+    meetingdate=models.DateField()
+    meetingtime=models.TimeField()
+    meetinglocation=models.CharField(max_length=255)
+    meetingagenda=models.TextField(null=True, blank=True) 
+
+    def __str__(self):
+        return self.meetingtitle
+
+    class Meta:
+        db_table='meeting'
+        verbose_name_plural='meetings'
+    
+class MeetingMinutes(models.Model):
+    meetingid=models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    attendance=models.ManyToManyField(User)
+    minutes=models.TextField()
+
+    def __str__(self):
+        return self.meetingid
+
+    class Meta:
+        db_table='meetingminutes'
+        verbose_name_plural='meetingminutesplural'
+
+class Resource(models.Model):
+    resourcename=models.CharField(max_length=255)
+    resourcetype=models.CharField(max_length=255)
+    url=models.URLField()
+    dateentered=models.DateField()
+    userid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    description=models.TextField()
+
+    def __str__(self):
+        return self.resourcename
+
+    class Meta:
+        db_table='resource'
+        verbose_name_plural='resources'
+
+class Event(models.Model):
+    eventtitle=models.CharField(max_length=255)
+    eventlocation=models.CharField(max_length=255)
+    eventdate=models.DateField()
+    eventtime=models.TimeField()
+    description=models.TextField()
+    userid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.eventtitle
+
+    class Meta:
+        db_table='event'
+        verbose_name_plural='events'
 
 # Create your models here.
