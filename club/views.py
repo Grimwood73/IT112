b2b1d5dfa5caa83dba1a512855a203643dfff719
Grimwood73 <jonah.byrne@gmail.com>
2022-05-07 18:34:53 -0700

    Meeting Details commit

--- club/views.py
+++ club/views.py
@@ -1,4 +1,6 @@
+from math import prod
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
@@ -8,3 +10,14 @@ def index(request):
 def resources(request):
     resource_list=Resource.objects.all()
     return render(request, 'club/resources.html', {'resource_list': resource_list})
+
+def meetings(request):
+    meetings_list=Meeting.objects.all()
+    return render(request, 'club/meetings.html', {'meetings_list': meetings_list})
+
+def meetingdetails(request, id):
+    prod=get_object_or_404(Meeting, pk=id)
+    context={
+        'prod' : prod,
+    }
+    return render(request, 'club/meetingdetails.html', context=context)
