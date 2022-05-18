8a4e9766993c8fdad0ea6480b8e8549d356eb59e
Grimwood73 <jonah.byrne@gmail.com>
2022-05-17 20:48:42 -0700

    Test commit

--- club/tests.py
+++ club/tests.py
@@ -1,3 +1,48 @@
+from turtle import title
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Meeting, MeetingMinutes, Resource, Event
+import datetime
 
-# Create your tests here.
+
+class MeetingTest(TestCase):
+    def setUp(self):
+        self.type=Meeting(meetingtitle='TestMeeting')
+
+    def test_typestring(self):
+        self.assertEqual(str(self.type), 'TestMeeting')
+    
+    def test_tablename(self):
+        self.assertEqual(str(Meeting._meta.db_table), 'meeting')
+
+class ResourceTest(TestCase):
+    def setUp(self):
+        self.type=Resource(resourcename='TestResource')
+
+    def test_typestring(self):
+        self.assertEqual(str(self.type), 'TestResource')
+    
+    def test_tablename(self):
+        self.assertEqual(str(Resource._meta.db_table), 'resource')
+
+class EventTest(TestCase):
+    def setUp(self):
+        self.type=Event(eventtitle='TestEvent')
+
+    def test_typestring(self):
+        self.assertEqual(str(self.type), 'TestEvent')
+    
+    def test_tablename(self):
+        self.assertEqual(str(Event._meta.db_table), 'event')
+
+#class MeetingMinutes(TestCase):
+    #def setUp(self):
+            #self.type=Meeting(meetingtitle='TestMeeting')
+            #self.meetingminutes=MeetingMinutes(meetingid=self.type)
+
+    #def test_typestring(self):
+        #self.assertEqual(str(meetingminutes), 'TestMeeting')
+    
+    #def test_tablename(self):
+        #self.assertEqual(str(MeetingMinutes._meta.db_table), 'meetingminutes')
+
