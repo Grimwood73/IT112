fe7fdea0bd38a126b66562fcc864bfd81070094b
Grimwood73 <jonah.byrne@gmail.com>
2022-05-21 13:42:27 -0700

    Forms commit

--- club/views.py
+++ club/views.py
@@ -1,6 +1,8 @@
 from math import prod
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+
+from .forms import MeetingForm, ResourceForm
 from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
@@ -21,3 +23,29 @@ def meetingdetails(request, id):
         'prod' : prod,
     }
     return render(request, 'club/meetingdetails.html', context=context)
+
+def newMeeting(request):
+    form=MeetingForm
+
+    if request.method=='POST':
+        form=MeetingForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=True)
+            post.save()
+            form=MeetingForm()
+    else:
+        form=MeetingForm()
+    return render(request, 'club/newmeeting.html', {'form': form})
+
+def newResource(request):
+    form=ResourceForm
+
+    if request.method=='POST':
+        form=ResourceForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=True)
+            post.save()
+            form=ResourceForm()
+    else:
+        form=ResourceForm()
+    return render(request, 'club/newresource.html', {'form': form})
